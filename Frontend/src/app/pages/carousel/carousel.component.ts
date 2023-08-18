59b51932e95fa9f47df5b7c8e55e94f8171406ac
Mayuresh <mayuresh483@gmail.com>
2023-08-18 18:28:14 +0530

    Fixed Deployment issue on Render

--- Frontend/src/app/pages/carousel/carousel.component.ts
+++ Frontend/src/app/pages/carousel/carousel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css','../../../assets/bootstrap.min.css']
+  styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
 }
